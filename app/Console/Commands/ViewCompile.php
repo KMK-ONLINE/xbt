2b8a76a3b73d4440f2e1fe6dc5b57843ff1ff897
Fadhil Mandaga and Nofriandi Ramenta <pair+fadhil+nofriandi@kmkonline.co.id>
2015-09-14 16:13:01 +0700

    [finished #103296858] Create command to Compile all View files

---
+++ app/Console/Commands/ViewCompile.php
@@ -0,0 +1,39 @@
+<?hh
+namespace App\Console\Commands;
+
+use Config, View;
+
+class ViewCompile extends WoiCommand
+{
+    protected $signature = 'view:compile
+                           {--dry-run : Kering}';
+
+    public function process() {
+        $views = array_filter(array_merge(Config::get('view.sites'), Config::get('view.paths')), function($path) {
+            return is_string($path);
+        });
+
+        foreach ($views as $view_path) {
+            $dir = new \RecursiveDirectoryIterator($view_path);
+            $iterator = new \RecursiveIteratorIterator($dir);
+            foreach ($iterator as $view) {
+                if (!$view->isFile()) continue;
+                $path = $view->getPathName();
+
+                if ($this->option('dry-run')) {
+                    $this->info($view->getPathName());
+                } else {
+                    try {
+                        $engine = View::getEngineFromPath($path);
+                        $engine->getCompiler()->compile($path);
+                    } catch (\InvalidArgumentException $e) {
+                        $this->info('SKIP: ' . $path . ' ERROR: ' . $e->getMessage());
+                    } catch (\Exception $e) {
+                        $this->error('FILE: ' . $path . ' ERROR: ' . $e->getMessage());
+                    }
+                }
+            }
+        }
+    }
+
+}
