d28050b7c1f3da8591ee2afb021f480224b9cc6c
Fadhil Mandaga and Nofriandi Ramenta <pair+fadhil+nofriandi@kmkonline.co.id>
2016-04-14 13:45:55 +0700

    [#117030285] Add checker also to XHP Blade compiler, and update ViewCompile dry-run

--- app/Console/Commands/ViewCompile.php
+++ app/Console/Commands/ViewCompile.php
@@ -21,17 +21,17 @@ class ViewCompile extends WoiCommand
                 if (!$view->isFile()) continue;
                 $path = $view->getPathName();
 
-                if ($this->option('dry-run')) {
-                    $this->info($view->getPathName());
-                } else {
-                    try {
-                        $engine = View::getEngineFromPath($path);
-                        $engine->getCompiler()->compile($path);
-                    } catch (\InvalidArgumentException $e) {
-                        $this->info('SKIP: ' . $path . ' ERROR: ' . $e->getMessage());
-                    } catch (\Exception $e) {
-                        $this->error('FILE: ' . $path . ' ERROR: ' . $e->getMessage());
+                try {
+                    $compiler = View::getEngineFromPath($path)->getCompiler();
+                    if ($this->option('dry-run')) {
+                        $this->info($path . ' :: ' . get_class($compiler));
+                    } else {
+                        $compiler->compile($path);
                     }
+                } catch (\InvalidArgumentException $e) {
+                    $this->info('SKIP: ' . $path . ' ERROR: ' . $e->getMessage());
+                } catch (\Exception $e) {
+                    $this->error('FILE: ' . $path . ' ERROR: ' . $e->getMessage());
                 }
             }
         }
