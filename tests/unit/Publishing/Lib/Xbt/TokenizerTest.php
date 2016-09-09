8a44e21c837165e48076219058dd351b8955a4b3
Fadhil Mandaga <pair+fadhil@kmkonline.co.id>
2016-09-09 15:15:12 +0700

    [#129521621][system tools] componentize xhpize

--- tests/unit/Publishing/Lib/Xbt/TokenizerTest.php
+++ tests/unit/Publishing/Lib/Xbt/TokenizerTest.php
@@ -8,7 +8,7 @@ class TokenizerTest extends \PHPUnit_Framework_TestCase
 
     public function test_tokenize_returns_tokenstream()
     {
-        $source = 'echo <h1>This is an H1 tag</h1>';
+        $source = '<h1>This is an H1 tag</h1>';
         $tokenizer = new Tokenizer($source);
         $this->assertTrue($tokenizer->tokenize() instanceof TokenStream);
     }
